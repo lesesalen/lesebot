5365fa33b5d7232c6b5272f402a341b96ddd2549
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-05-19 20:40:58 +0200

    feat: add cat facts command

---
+++ src/commands/fun/catfacts.ts
@@ -0,0 +1,23 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { Message } from "discord.js";
+import axios from "axios";
+
+class InsultCommand extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: "catfact",
+      group: "fun",
+      memberName: "catfact",
+      description: "You know what you need? CAT FACTS",
+    });
+  }
+
+  run = async (message: CommandoMessage): Promise<Message | Message[]> => {
+    const api = await axios.get(`https://catfact.ninja/fact`);
+    const fact = api.data.fact;
+
+    return await message.say(`Did you know? ${fact}`);
+  };
+}
+
+export default InsultCommand;
