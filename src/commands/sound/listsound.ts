99a31ee261d7a6a24e1b201fcb43a7c0f8296a2b
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-05-19 22:29:46 +0200

    feat: add sound and sound list command

---
+++ src/commands/sound/listsound.ts
@@ -0,0 +1,31 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { Message } from "discord.js";
+import globby from "globby";
+import path from "path";
+
+class ListSoundCommand extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: "listsound",
+      aliases: ["ls"],
+      group: "sound",
+      memberName: "listsound",
+      description: "List of all the available 'sounds'",
+    });
+  }
+
+  run = async (message: CommandoMessage): Promise<Message | Message[]> => {
+    await message.direct("Here are all the sound files!");
+    const paths = await globby(`${path.resolve(process.cwd(), "assets")}/*.mp3`);
+    const reply = paths
+      .map((p) => {
+        const { name, ext } = path.parse(p);
+        return `> ${name}${ext}`;
+      })
+      .filter((p) => p !== "> wednesday.mp3")
+      .join("\n");
+    return await message.direct(reply);
+  };
+}
+
+export default ListSoundCommand;
---
+++ src/commands/sound/sound.ts
@@ -0,0 +1,55 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { Message } from "discord.js";
+import fs from "fs";
+import path from "path";
+
+class SoundCommand extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: "sound",
+      aliases: ["audio", "noise"],
+      group: "sound",
+      memberName: "sound",
+      description: "Play some nice, calming sounds for your channel",
+      argsPromptLimit: 0,
+      args: [
+        {
+          key: "file",
+          prompt: "MP3 to play",
+          type: "string",
+          validate: undefined,
+          default: "",
+        },
+      ],
+    });
+  }
+
+  run = async (message: CommandoMessage, { file }: { file: string }): Promise<Message | Message[]> => {
+    if (file === "") {
+      return await message.reply("You need to specify the file to play...");
+    }
+    if (file === "wednesday") {
+      return await message.reply("hey, that's illegal");
+    }
+
+    const voiceChannel = message.member.voice.channel;
+
+    if (!voiceChannel) {
+      return await message.reply("You need to be in a voice channel for THE GONG");
+    } else {
+      const connection = await voiceChannel.join();
+      const dispatcher = connection.play(fs.createReadStream(path.resolve(process.cwd(), `assets/${file}.mp3`)));
+      dispatcher.on("error", console.error);
+      dispatcher.on("start", () => console.log(`${file}.mp3 is now playing`));
+      dispatcher.on("finish", () => {
+        console.log(`${file}.mp3 is done playing`);
+        dispatcher.destroy();
+        voiceChannel.leave();
+      });
+    }
+
+    return await message.direct("I hope you're happy now...");
+  };
+}
+
+export default SoundCommand;
