5e37b4b5668162ce79947e1948892a52a90b9cf6
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-05-19 21:59:12 +0200

    feat: its wednesday my dude

---
+++ src/commands/sound/wednesday.ts
@@ -0,0 +1,41 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { Message } from "discord.js";
+import dateformat from "dateformat";
+import fs from "fs";
+import path from "path";
+
+class WednesdayCommand extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: "wednesday",
+      group: "sound",
+      memberName: "wednesday",
+      description: "It is Wednesday my Dudes",
+    });
+  }
+
+  run = async (message: CommandoMessage): Promise<Message | Message[]> => {
+    if (dateformat(new Date(), "dddd").toLowerCase() !== "wednesday") {
+      return await message.say("It is **NOT** Wednesday my Dudes");
+    }
+
+    const voiceChannel = message.member.voice.channel;
+
+    if (!voiceChannel) {
+      return await message.reply("You need to be in a voice channel for wednesday");
+    } else {
+      const connection = await voiceChannel.join();
+      const dispatcher = connection.play(fs.createReadStream(path.resolve(process.cwd(), "assets/wednesday.mp3")));
+      dispatcher.on("error", console.error);
+      dispatcher.on("start", () => console.log("wednesday.mp3 is now playing"));
+      dispatcher.on("finish", () => {
+        console.log("wednesday.mp3 is done playing");
+        dispatcher.destroy();
+        voiceChannel.leave();
+      });
+      return await message.say("It is Wednesday my Dudes");
+    }
+  };
+}
+
+export default WednesdayCommand;
