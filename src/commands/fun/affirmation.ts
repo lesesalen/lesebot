916d17e009aa26826fef3b7d0832d499b567d879
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-09-12 23:42:22 +0200

    feat: add @ for !affirm command

--- src/commands/fun/affirmation.ts
+++ src/commands/fun/affirmation.ts
@@ -1,5 +1,5 @@
 import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
-import { Message } from "discord.js";
+import { Message, User } from "discord.js";
 import axios from "axios";
 import logger from "../../utils/logger";
 
@@ -11,19 +11,38 @@ class AffirmationCommand extends Command {
       group: "fun",
       memberName: "affirmation",
       description: "We all need that little friendly push",
+      argsPromptLimit: 0,
+      args: [
+        {
+          key: "target",
+          prompt: "The user to target",
+          type: "user",
+          validate: undefined,
+          default: "",
+        },
+      ],
     });
   }
 
-  run = async (message: CommandoMessage): Promise<Message | Message[]> => {
+  run = async (message: CommandoMessage, { target }: { target: User | string }): Promise<Message | Message[]> => {
     const api = await axios.get<Record<string, string>>(`https://www.affirmations.dev/`);
     const affirm = api.data.affirmation;
 
     logger.info({
       message: "Someone needed some affirmation...",
       userId: message.author.id,
+      targetId: target.toString(),
     });
 
-    return await message.say(affirm);
+    if (typeof target === "string") {
+      return await message.say(`${affirm}.`);
+    } else {
+      if (target.id === message.author.id) {
+        return await message.reply(`Need a little lift, <@${target.id}>? ${affirm}.`);
+      }
+    }
+
+    return await message.say(`<@${target.id}>: ${affirm}. (from <@${message.author.id}>)`);
   };
 }
 
