8b5d9f33aef74f23bbc2fd07b2ee65eeed6ccc0f
Anders Syvertsen <andesyv@gmail.com>
2020-10-07 10:41:57 +0200

    Spooky command (#27)

---
+++ src/commands/fun/spook.ts
@@ -0,0 +1,56 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { Message, MessageEmbed, User } from "discord.js";
+import logger from "../../utils/logger";
+import axios from "axios";
+
+interface giphyrespone {
+  data: {
+    images: {
+      original: {
+        url: string;
+      };
+    };
+  };
+}
+
+class SpookCommand extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: "spook",
+      aliases: ["spooky", "spooks", "skeleton"],
+      group: "fun",
+      memberName: "spook",
+      description: "Sends a random spooky skeleton!",
+      details: `Returns a random gif from giphy with the "skeleton" tag \
+and sends it in the current channel, or to an optional user \
+if @target is specified.`,
+      args: [
+        {
+          key: "target",
+          prompt: "An optional user to spook",
+          type: "user",
+          validate: undefined,
+          default: "",
+        },
+      ],
+    });
+  }
+
+  run = async (message: CommandoMessage, { target }: { target: User }): Promise<Message | Message[]> => {
+    const gif = await axios.get<giphyrespone>(
+      `https://api.giphy.com/v1/gifs/random?tag=skeleton&api_key=${String(process.env.GIPHY_API_KEY)}`,
+    );
+    const url = gif.data.data.images.original.url;
+    const embed = new MessageEmbed().setImage(url);
+
+    logger.info({
+      message: `Spooked with ${url}`,
+      userId: message.author.id,
+    });
+
+    if (target) return await target.send(embed.setTitle(`Spooked by ${message.author.tag}!`));
+    else return await message.say(embed);
+  };
+}
+
+export default SpookCommand;
