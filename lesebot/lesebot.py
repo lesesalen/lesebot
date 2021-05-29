4d56c5dee6b621f099ad3c0e3bc3b7d5ade31608
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-05-29 17:40:07 +0200

    And we have liftoff!

---
+++ lesebot/lesebot.py
@@ -0,0 +1,22 @@
+import os
+
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
+
+load_dotenv()
+
+bot = commands.Bot(command_prefix=os.getenv("DISCORD_PREFIX"), intents=discord.Intents.all())
+slash = SlashCommand(bot, sync_commands=True)
+
+
+@slash.slash(name="test")
+async def test_command(ctx: SlashContext):
+    embed = discord.Embed(title="Test")
+    await ctx.send(content="test", embeds=[embed])
+
+
+if __name__ == '__main__':
+    print("Bot online!")
+    bot.run(os.getenv("DISCORD_TOKEN"))
