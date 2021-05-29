ab5c68a7e3eb0041d133c39ad882eecdf705cb8a
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-05-29 18:21:19 +0200

    Add logging to bot, add isort as dev dependency

--- lesebot/lesebot.py
+++ lesebot/lesebot.py
@@ -1,9 +1,18 @@
+import logging
 import os
 
-from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
+from dotenv import load_dotenv
+
+logger = logging.getLogger("discord")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+handler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
+logger.addHandler(handler)
 
 load_dotenv()
 
