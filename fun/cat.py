e4baf2e01b679c56807f2fc27a1a1ee95fa22a80
DrakePork <drakepork@hotmail.com>
2021-08-16 06:31:59 +0200

    Added cat command

---
+++ fun/cat.py
@@ -0,0 +1,32 @@
+import requests
+import discord
+from discord.ext import commands
+from discord_slash import SlashContext, cog_ext
+
+
+async def make(ctx):
+    res = requests.get("https://api.thecatapi.com/v1/images/search")
+    catUrl = res.json()[0]["url"]
+    embed = discord.Embed(title="Cat")
+    embed.set_image(url=catUrl)
+    await ctx.send(embed=embed)
+
+
+class CatCommand(commands.Cog):
+    def __init__(self, client: commands.Bot):
+        self.client = client
+
+    @commands.command()
+    async def cat(self, ctx: SlashContext):
+        await make(ctx)
+
+    @cog_ext.cog_slash(
+        name="cat",
+        description="Caaat",
+    )
+    async def _cat(self, ctx: SlashContext):
+        await make(ctx)
+
+
+def setup(client: commands.Bot):
+    client.add_cog(CatCommand(client))
