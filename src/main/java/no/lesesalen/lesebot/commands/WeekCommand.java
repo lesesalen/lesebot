66d59b9782083e4149fcf1534b9fa69bec0f00c9
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-10-11 22:26:37 +0200

    Fix week command not registering, make it look like old version

--- src/main/java/no/lesesalen/lesebot/commands/WeekCommand.java
+++ src/main/java/no/lesesalen/lesebot/commands/WeekCommand.java
@@ -3,11 +3,16 @@ package no.lesesalen.lesebot.commands;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.rest.util.Color;
+import no.lesesalen.lesebot.utils.Constants;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.temporal.WeekFields;
 import java.util.Date;
 
+@Component
 public class WeekCommand implements SlashCommand {
     @Override
     public String getName() {
@@ -16,18 +21,16 @@ public class WeekCommand implements SlashCommand {
 
     @Override
     public Mono<Void> handle(ChatInputInteractionEvent event) {
+        var week = LocalDate.now().get(WeekFields.ISO.weekOfYear());
         var embed = EmbedCreateSpec.builder()
                 .color(Color.of(0x0099FF))
-                .image("attachment://assets/blobross.png")
                 .title("Week number")
-                .description(new Date().toString())
-                .thumbnail("")
+                .description(Integer.toString(week))
+                .thumbnail(Constants.BLOBROSS)
                 .timestamp(Instant.now())
                 .footer("Don't you have a calendar?", "")
                 .build();
 
-        return event.reply()
-                .withEphemeral(true)
-                .withEmbeds(embed);
+        return event.reply().withEmbeds(embed);
     }
 }
---
+++ src/main/java/no/lesesalen/lesebot/utils/Constants.java
@@ -0,0 +1,8 @@
+package no.lesesalen.lesebot.utils;
+
+public class Constants {
+    public static final String BLOBROSS = "https://raw.githubusercontent.com/lesesalen/lesebot/master/assets/blobross.png";
+
+    private Constants() {
+    }
+}
