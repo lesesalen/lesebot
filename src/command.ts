0e35623d7a9a8a724b4c89e925af4215c1f6e292
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-10-17 22:14:35 +0200

    Proper typed imports of commands, renaming and refactors

--- src/command.ts
+++ src/command.ts
@@ -3,31 +3,29 @@ import { ApplicationCommandTypes } from "discord.js/typings/enums";
 
 import { DiscordClient } from "./client";
 
-export interface ICommand {
+export type CommandConstructor = { default: { new (): SlashCommand } };
+
+export interface SlashCommand extends ChatInputApplicationCommandData {
   handle(interaction: CommandInteraction, client: DiscordClient): Promise<void>;
   toJson(): ChatInputApplicationCommandData;
 }
 
-export abstract class Command implements ChatInputApplicationCommandData {
-  name: string;
-  description: string;
+export abstract class CommandHandler implements SlashCommand {
+  name!: string;
+  description!: string;
   type?: "CHAT_INPUT" | ApplicationCommandTypes.CHAT_INPUT | undefined;
   options?: ApplicationCommandOptionData[] | undefined;
   defaultPermission?: boolean | undefined;
 
-  constructor(options: ChatInputApplicationCommandData) {
-    this.name = options.name;
-    this.description = options.description;
-    this.type = options.type;
-    this.defaultPermission = options.defaultPermission;
-    this.options = options.options;
+  protected constructor(options: ChatInputApplicationCommandData) {
+    Object.assign(this, options);
   }
 
   public abstract handle(interaction: CommandInteraction, client: DiscordClient): Promise<void>;
 
   public toJson(): ChatInputApplicationCommandData {
     return {
-      name: this.name,
+      name: this.name.toLowerCase(),
       description: this.description,
       type: this.type,
       options: this.options ?? [],
