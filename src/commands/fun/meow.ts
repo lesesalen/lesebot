781be5ecb79c06588068904b2b330d92eb824c3a
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-10-19 21:19:17 +0200

    Refactor SlashCommandHandler to use SlashCommandBuilder

--- src/commands/fun/meow.ts
+++ src/commands/fun/meow.ts
@@ -1,13 +1,11 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
 
 import { DiscordClient, SlashCommandHandler } from "../../client";
 
 export default class MeowCommand extends SlashCommandHandler {
   constructor() {
-    super({
-      name: "Meow",
-      description: "Replies with a meaw, kitty cat",
-    });
+    super(new SlashCommandBuilder().setName("meow").setDescription("Replies with a meaw, kitty cat"));
   }
 
   handle(interaction: CommandInteraction, _client: DiscordClient): Promise<void> {
