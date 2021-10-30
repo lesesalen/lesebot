5f254437926b773d5c8d9d21c57df5b1d73a2d13
Anders Syvertsen <andesyv@gmail.com>
2021-10-30 17:33:39 +0200

    Converted rigged

---
+++ src/commands/calendar/rigged.ts
@@ -0,0 +1,14 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+
+import { DiscordClient, SlashCommandHandler } from "../../client";
+
+export default class RiggedCommand extends SlashCommandHandler {
+  builder = new SlashCommandBuilder().setName("rigged").setDescription("YOU DIDN'T WIN?! MUST BE A RIGGED SYSTEM");
+
+  async handle(interaction: CommandInteraction, _client: DiscordClient): Promise<void> {
+    return interaction.reply(
+      `En feil har oppstått. Grunnet lav tilgang på økonomiske ressurser trenger vi hjelp til å ordne opp i feilen. Vi tar imot _frivillige donasjoner_ på **2188** på vipps.`,
+    );
+  }
+}
