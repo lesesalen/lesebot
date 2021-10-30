87e6bf78b77b89cc403ba0b2ce5254640b175104
Anders Syvertsen <andesyv@gmail.com>
2021-10-31 01:15:26 +0200

    Started on calendar conversion

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,20 @@
+import {
+  AudioPlayer,
+  AudioPlayerStatus,
+  createAudioResource,
+  entersState,
+  joinVoiceChannel,
+  StreamType,
+  VoiceConnection,
+  VoiceConnectionStatus,
+} from "@discordjs/voice";
 import axios from "axios";
+import { VoiceChannel } from "discord.js";
 import { promises as fs } from "fs";
-import { globby } from "globby";
 import path from "path";
 
 import { RandomGenerateIntegers } from "../types";
+import { createDiscordJSAdapter } from "./adapter";
 import logger from "./logger";
 
 export const writeJson = async <T>(filePath: string, content: T): Promise<void> => {
@@ -47,12 +58,13 @@ export const sample = <T>(array: T[]): T => {
   return array[Math.floor(Math.random() * array.length)];
 };
 
+const getPathsByExtension = async (folder: string, extension: string): Promise<string[]> => {
+  const dir = await fs.readdir(folder);
+  return dir.filter((p) => path.extname(p) === extension).map((p) => path.resolve(process.cwd(), folder, p));
+};
+
 export const soundSamples = async (): Promise<string[]> => {
-  const paths = await globby(`${path.resolve(process.cwd(), "assets")}/*.mp3`);
-  return paths.map((p) => {
-    const { name } = path.parse(p);
-    return name;
-  });
+  return getPathsByExtension("assets", ".mp3");
 };
 
 export const jsonToMap = <V>(jsonString: string): Map<string, V> => {
@@ -94,4 +106,47 @@ export const randomNumber = async (min: number, max: number): Promise<number> =>
   });
 
   return request.data.result.random.data[0];
+};
+
+export const createVoiceChannelConnection = async (voiceChannel: VoiceChannel): Promise<VoiceConnection> => {
+  const connection = joinVoiceChannel({
+    channelId: voiceChannel.id,
+    guildId: voiceChannel.guild.id,
+    adapterCreator: createDiscordJSAdapter(voiceChannel),
+    debug: true,
+  });
+
+  console.log(connection.state);
+
+  try {
+    /**
+     * Allow ourselves 30 seconds to join the voice channel. If we do not join within then,
+     * an error is thrown.
+     */
+    await entersState(connection, VoiceConnectionStatus.Ready, 30e3);
+    /**
+     * At this point, the voice connection is ready within 30 seconds! This means we can
+     * start playing audio in the voice channel. We return the connection so it can be
+     * used by the caller.
+     */
+    return connection;
+  } catch (error) {
+    /**
+     * At this point, the voice connection has not entered the Ready state. We should make
+     * sure to destroy it, and propagate the error by throwing it, so that the calling function
+     * is aware that we failed to connect to the channel.
+     */
+    connection.destroy();
+    throw error;
+  }
+};
+
+export const playSong = (player: AudioPlayer, songpath: string) => {
+  const resource = createAudioResource(songpath, {
+    inputType: StreamType.Arbitrary,
+  });
+  if (!resource || !resource.readable) throw `Cannot read ${songpath}`;
+  player.play(resource);
+  console.log(player.checkPlayable());
+  return entersState(player, AudioPlayerStatus.Playing, 5e3);
 };
