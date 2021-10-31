1204b2db3373def684c35e61a678ca93b5765b45
L0SER <65214509+L0SER8228@users.noreply.github.com>
2021-11-01 00:02:13 +0530

    Create meme.js

---
+++ src/commands/fun/meme.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import Commando from "discord.js-commando";
+
+import logger from "../../utils/logger.js";
+
+class MemeCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "meme",
+      group: "fun",
+      memberName: "meme",
+      description: "Funny meme, ha ha",
+    });
+  }
+
+  async run(message) {
+    const api = await axios.get(`https://meme-api.herokuapp.com/gimme`, {
+      headers: {
+        "User-Agent": "lesebot (https://github.com/lesesale/lesebot)",
+        Accept: "application/json",
+      },
+    });
+
+    const joke = api.data.url;
+
+    logger.info({
+      message: "Funny meme, ha ha",
+      userId: message.author.id,
+    });
+
+    return await message.say(url);
+  }
+}
+
+export default DadJokeCommand;
