c8cfa5b010dba72914d82721b76d05d215955eac
Anders Syvertsen <andesyv@gmail.com>
2021-10-31 02:45:40 +0200

    Dadjoke (JS)

--- src/commands/fun/dadjoke.js
+++ src/commands/fun/dadjoke.js
@@ -1,36 +1,19 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
 import axios from "axios";
-import Commando from "discord.js-commando";
 
-import logger from "../../utils/logger.js";
+import { SlashCommandHandler } from "../../client";
 
-class DadJokeCommand extends Commando.Command {
-  constructor(client) {
-    super(client, {
-      name: "dadjoke",
-      aliases: ["kenneth"],
-      group: "fun",
-      memberName: "dadjoke",
-      description: "DAD YOKE, ha ha ha",
-    });
-  }
+export default class DadJokeCommand extends SlashCommandHandler {
+  builder = new SlashCommandBuilder().setName("dadjoke").setDescription("DAD YOKE, ha ha ha"); //.setAlias("kenneth")
 
-  async run(message) {
-    const api = await axios.get(`https://icanhazdadjoke.com/`, {
+  handle(interaction, _client) {
+    axios.get(`https://icanhazdadjoke.com/`, {
       headers: {
         "User-Agent": "lesebot (https://github.com/lesesale/lesebot)",
         Accept: "application/json",
       },
+    }).then(({ data: { joke }}) => {
+      return interaction.reply(joke);
     });
-
-    const joke = api.data.joke;
-
-    logger.info({
-      message: "Dad joke, ha ha",
-      userId: message.author.id,
-    });
-
-    return await message.say(joke);
   }
 }
-
-export default DadJokeCommand;
