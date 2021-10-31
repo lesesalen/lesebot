a690c5acab8c80ecb526b82c01d41177a4dad0d9
Anders Syvertsen <andesyv@gmail.com>
2021-10-31 17:57:21 +0100

    Week command

---
+++ src/commands/utils/week.ts
@@ -0,0 +1,25 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { format } from "date-fns";
+import { CommandInteraction, MessageAttachment, MessageEmbed } from "discord.js";
+import path from "path";
+
+import { DiscordClient, SlashCommandHandler } from "../../client";
+
+export default class WeekCommand extends SlashCommandHandler {
+  builder = new SlashCommandBuilder()
+    .setName("week") // .setAlias("uke", "ukenr")
+    .setDescription("What week is it again? Man, I wish I had a calendar");
+
+  async handle(interaction: CommandInteraction, _client: DiscordClient): Promise<void> {
+    const attachment = new MessageAttachment(path.resolve(process.cwd(), "assets/blobross.png"));
+    const embed = new MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle("Week number")
+      .setDescription(format(new Date(), "w"))
+      .setThumbnail("attachment://blobross.png")
+      .setTimestamp()
+      .setFooter("Don't you have a calendar?");
+
+    return await interaction.reply({ embeds: [embed], files: [attachment] });
+  }
+}
