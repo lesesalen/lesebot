3cd9dceae2a6fc1e9142a66fac2514161e557487
Sondre Nilsen <sondre@eons.io>
2021-11-03 16:18:29 +0100

    Update deploy script, enable deploying bot
    This closes #52

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ echo "Getting latest Docker container"
 docker pull ghcr.io/lesesalen/lesebot:latest
 
 echo "Stopping bot..."
-docker stop lesebot
+docker stop testbot
 
 echo "Removing old container..."
-docker container rm lesebot
+docker container rm testbot
 
 echo "Redeploying bot"
-docker run --env-file .env --pull always -itd --restart unless-stopped --name lesebot ghcr.io/lesesalen/lesebot:latest
+docker run --env-file .env --pull always -itd --restart unless-stopped --name testbot ghcr.io/lesesalen/lesebot:latest
 
 echo "And we're live again!"
